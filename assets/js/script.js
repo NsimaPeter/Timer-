564792aaaf626e0f8314d3f31d6326a79cafcbbc
NsimaPeter <>
2023-05-01 22:04:11 +0000

    add functionality using javascrit for buttons and also add eventlistener click commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let startEl = document.getElementById("start")
+let stopEl = document.getElementById("stop")
+let resetEl = document.getElementById("start")
+let timerEl = document.getElementById("timer")
+
+
+
+function startTimer(){
+    console.log("start");
+}
+
+
+
+startEl.addEventListener("click", startTimer)
